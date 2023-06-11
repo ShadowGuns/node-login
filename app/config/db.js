df311ddecdf18ba9c797ff26d6d6c83c7cb2e01c
ShadowGuns <vol6921@gmail.com>
2023-06-11 16:55:44 +0700

    update

--- app/config/db.js
+++ app/config/db.js
@@ -25,31 +25,3 @@ module.exports={
     pool
 }
 
-// module.exports ={
-//     HOST: "arjuna.db.elephantsql.com",
-//     USER: "swxlxmzn",
-//     PASSWORD: "kCoMR-qLBjF_lt_aW54Mn3cPSIpfXENh",
-//     DB: "swxlxmzn",
-//     dialect: "postgres",
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000, // 30 seconds
-//         idle: 10000 // 10 seconds
-//     }
-// };
-
-// module.exports ={
-//     HOST: "localhost",
-//     USER: "postgres",
-//     PASSWORD: "workhelp1@",
-//     DB: "system",
-//     dialect: "postgres",
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000, // 30 seconds
-//         idle: 10000 // 10 seconds
-//     }
-// };
-
